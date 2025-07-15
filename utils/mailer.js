b20d121c8aee2fa5c9166fd200dc10fe429c178f
Hapid12 <buttertiger022@gmail.com>
2025-07-15 11:03:14 +0700

    new update 1

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,10 +1,12 @@
+require('dotenv').config(); // tambahkan baris ini jika belum
+
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.MY_GMAIL,
-    pass: process.env.MY_GMAIL_PASSWORD
+    pass: process.env.MY_GMAIL_PASS
   }
 });
 
